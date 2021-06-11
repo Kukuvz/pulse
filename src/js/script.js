142588327c4ceeef381eb7a5d51d606a01802816
Anton Gaivoronskiy <gaivoronskiy.anton.i@gmail.com>
2021-06-11 09:59:07 +0300

    add php mailer

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+
+	// Slick slider
+
     $('.carousel__inner').slick({
         speed: 800,
         adaptiveHeight: false,
@@ -32,6 +35,8 @@ $(document).ready(function(){
           ]
     });
     
+	//Tabs
+
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
         $(this)
           .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
@@ -51,4 +56,89 @@ $(document).ready(function(){
     
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
+
+  	// Modal 
+
+	$('[data-modal=consultation]').on('click', function() {
+		$('.overlay, #consultation').fadeIn('slow');
+	});
+	$('.modal__close').on('click', function() {
+		$('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+	});
+
+	$('.button_mini').each(function(i) {
+		$(this).on('click', function() {
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay, #order').fadeIn('slow');
+		});
+	});
+
+	// Validate
+
+	function validForms(form) {
+		$(form).validate({
+			rules: {
+				name: {
+					required: true,
+					minlength: 2
+				},
+				phone: "required",
+				email: {
+					required: true,
+					email: true
+				}
+			},
+			messages: {
+				name: {
+					required: "Пожалуйста, введите свое имя",
+					minlength: jQuery.validator.format("Введите {0} символа!")
+				},
+				phone: "Пожалуйста, введите свой номер телефона",
+				email: {
+				  required: "Пожалуйста, введите свою почту",
+				  email: "Неправильно введен адрес почты"
+				}
+			}
+		});
+	}
+
+	validForms('#consultation-form');
+	validForms('#consultation form');
+	validForms('#order form');
+
+	//Mask phone
+
+	$('input[name=phone]').mask("+7 (999) 999-99-99");
+
+	$('form').submit(function(e) {
+		e.preventDefault();
+		$.ajax({
+			type: "POST",
+			url: "mailer/smart.php",
+			data: $(this).serialize()
+		}).done(function() {
+			$(this).find("input").val("");
+			$('#consultation, #order').fadeOut();
+			$('.overlay, #thanks').fadeIn('slow');
+			$('form').trigger('reset');
+		});
+		return false;
+	});
+
+	// Smooth scroll & pageup
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 1600) {
+			$('.pageup').fadeIn();
+		} else {
+			$('.pageup').fadeOut();
+		}
+	});
+
+	$("a[href^='#']").click(function(){
+		const _href = $(this).attr("href");
+		$("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+		return false;
+	});
+	
 });
